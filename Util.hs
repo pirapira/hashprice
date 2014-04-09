42614fe26050ba1573d9c6db7e4ab20c5c9f1355
Yoichi Hirai <i@yoichihirai.com>
2014-04-09 23:49:42 +0900

    can read ticker from file

--- Util.hs
+++ Util.hs
@@ -2,6 +2,10 @@ module Util where
 
 import Import
 import Data.Int (Int64)
+import Control.Monad (mzero)
+import Data.Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Data.ByteString.Lazy as B
 
 blockReward :: Block -> Int64
 blockReward block =
@@ -16,3 +20,32 @@ reward_ind height init =
   if height < 210000
   then init
   else reward_ind (height - 210000) $ init `div` 2
+
+data TickResult = TickResult !Object deriving (Show)
+
+instance FromJSON TickResult where
+  parseJSON (Object v) = TickResult <$> v .: "USD"
+  parseJSON _ = mzero
+
+data Tick = Tick Text deriving (Show)
+
+instance FromJSON Tick where
+  parseJSON (Object v) = Tick <$> v .: "last"
+  parseJSON _ = mzero
+
+jsonFile :: FilePath
+jsonFile = "./ticker.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+tickerResult :: IO (Either String (HM.HashMap Text Text))
+tickerResult = (eitherDecode <$> getJSON)
+
+tick :: IO (Maybe Tick)
+tick = do
+  outer <- tickerResult
+  case outer of
+    Left _ -> return $ Nothing
+    Right out ->
+      return $ fmap Tick $ HM.lookup "last" out
