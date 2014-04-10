1989401b0727ad18d45316cdf0f4d0666276f47c
Yoichi Hirai <i@yoichihirai.com>
2014-04-10 09:26:24 +0900

    get and save ticker

--- Model.hs
+++ Model.hs
@@ -6,6 +6,7 @@ import Data.Int (Int64)
 import Data.Text (Text)
 import Database.Persist.Quasi
 import Data.Typeable (Typeable)
+import Data.Time.Clock
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
--- Util.hs
+++ Util.hs
@@ -1,9 +1,12 @@
 module Util where
 
+-- use whateverTick
+
 import Import
 import Data.Int (Int64)
 import Control.Monad (mzero)
 import Data.Aeson
+import Data.Time.Clock
 
 import Data.Scientific as Scientific
 import qualified Data.HashMap.Strict as HM
@@ -28,6 +31,9 @@ reward_ind height init =
 data TickResult = TickResult !Object deriving (Show)
 
 data Tick = Tick Rational deriving (Show)
+
+unTick :: Tick -> Rational
+unTick (Tick r) = r
 
 instance FromJSON Tick where
   parseJSON (Object v) = Tick <$> v .: "last"
@@ -70,4 +76,44 @@ tick = do
   case prev of
     Nothing -> return Nothing
     Just p -> return $ l2t p
+
+fiveMinAgo :: Handler UTCTime
+fiveMinAgo = do
+  curTime <- liftIO getCurrentTime
+  return $ addUTCTime (5 * 60) curTime
+
+recentTick :: Handler (Maybe Tick)
+recentTick = do
+  thr <- fiveMinAgo
+  result <- runDB $ selectFirst [TickerCreatedAt >=. thr] [Desc TickerCreatedAt]
+  case result of
+    Nothing -> return $ Nothing
+    Just (Entity _ ticker) ->
+      return $ Just $ Tick (tickerUsdbtc ticker)
+
+saveTick :: Tick -> Handler TickerId
+saveTick t = do
+  current <- liftIO getCurrentTime
+  runDB $ insert
+    $ Ticker
+    { tickerUsdbtc = unTick t
+    , tickerCreatedAt = current
+    }
+
+getSaveTick :: Handler Tick
+getSaveTick = do
+  t <- liftIO tick
+  case t of
+    Nothing -> redirect $ HomeR
+    Just tic -> do
+      _ <- saveTick tic
+      return tic
+
+whateverTick :: Handler Tick
+whateverTick = do
+  recent <- recentTick
+  recentJust <- case recent of
+              Just tic -> return tic
+              Nothing -> getSaveTick
+  return recentJust
 
